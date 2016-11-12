18309bc6bc049ab49526072e7debfef8f41d9208
thoma287 <Stevenisdrew@Drews-MacBook-Pro-6.local>
2016-11-12 14:10:34 -0800

    Added Create Event and Org Login front page template

---
+++ iOS/Ordinem/CreateEvent.swift
@@ -0,0 +1,13 @@
+//
+//  CreateEvent.swift
+//  Ordinem
+//
+//  Created by Drew Thomas on 11/12/16.
+//  Copyright © 2016 Ordinem. All rights reserved.
+//
+
+import UIKit
+
+class CreateEvent: NSObject {
+
+}

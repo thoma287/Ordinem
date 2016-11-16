c72b375af2feadfdc7c3cbdf203c9176c15f925a
thoma287 <Stevenisdrew@Drews-MBP-6.attlocal.net>
2016-11-16 00:54:45 -0800

    mas

---
+++ iOS/Ordinem/UserNewsFeed.swift
@@ -0,0 +1,53 @@
+//
+//  UserNewsFeed.swift
+//  Ordinem
+//
+//  Created by Drew Thomas on 11/16/16.
+//  Copyright © 2016 Ordinem. All rights reserved.
+//
+
+import UIKit
+
+class UserNewsFeed: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
+
+class UserHeaderDateNewsFeed : UITableViewCell{
+    
+    @IBOutlet weak var HeaderDate4UserNewsFeed: UILabel!
+    
+    
+}
+
+class UserTemplateCell4NewsFeed : UITableViewCell{
+    
+    
+    
+    
+    
+    
+    
+}

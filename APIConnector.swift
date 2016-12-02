8b161171e3ef261c4b6a69f668458afa8e6aa601
Shevis Johnson <johns612@mail.chapman.edu>
2016-12-02 01:56:08 -0800

    Adds poster and some other stuff

---
+++ APIConnector.swift
@@ -0,0 +1,21 @@
+//
+//  APIConnector.swift
+//  Ordinem
+//
+//  Created by Shevis Johnson on 12/1/16.
+//  Copyright © 2016 Ordinem. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class APIConnector {
+    
+    
+    
+    func orgLogin(email: String, password: String, databaseName: String) {
+        
+    }
+    
+    
+}
